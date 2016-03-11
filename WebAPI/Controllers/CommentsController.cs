5b6564d0a6443d43bdbe74a46e32eadf877ed004
hoangtungitbk <hoangtungitbk@gmail.com>
2016-03-11 13:54:20 +0700

    first commit

---
+++ WebAPI/Controllers/CommentsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class CommentsController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<Comment> Get()
+        {
+            return null;
+        }
+
+        // GET api/values/5
+        public Comment Get(int id)
+        {
+            return null;
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
